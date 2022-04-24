4d5a0a220302c6807907bdb41800bd056247bf7a
Resinchen <alex1410alex@gmail.com>
2022-04-24 12:39:11 +0500

    lint2

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,27 +1,28 @@
+import json
 import os
-
-from lrparser import Parser, Tokenizer
-from config import character, scene
-from lrparser.utils.parser import Attribute
 from os import path
 
-import json
+from config import character, scene
+from lrparser import Parser, Tokenizer
+from lrparser.utils.parser import Attribute
+
 
 class ParserBlock:
     def __init__(self, tokens, actions, goto):
         self.tokenizer = Tokenizer(tokens)
         self.parser = Parser(actions, goto)
 
+
 class Compiler:
     def __init__(self):
         self.parsers: dict[str, ParserBlock] = {
-            'char': ParserBlock(character.tokens, character.actions, character.goto),
-            'scene': ParserBlock(scene.tokens, scene.actions, scene.goto)
+            "char": ParserBlock(character.tokens, character.actions, character.goto),
+            "scene": ParserBlock(scene.tokens, scene.actions, scene.goto),
         }
 
     def build_game(self, game_directory: str):
-        script_folder = path.join(game_directory, 'script')
-        compiled_folder = path.join(game_directory, 'compiled')
+        script_folder = path.join(game_directory, "script")
+        compiled_folder = path.join(game_directory, "compiled")
         character_file, scene_files = self._get_files(script_folder)
 
         if not os.path.exists(compiled_folder):
@@ -32,30 +33,38 @@ class Compiler:
             self._compile_scene_file(file, compiled_folder)
 
     def _get_files(self, script_path: str) -> tuple[str, list[str]]:
-        get_abspath = lambda fp: path.join(path.abspath(script_path), fp)
         filenames = os.listdir(script_path)
-        filenames = [get_abspath(filepath) for filepath in filenames]
+        filenames = [self._get_abspath(filepath, script_path) for filepath in filenames]
 
-        char_filename = [filename for filename in filenames if 'chars'in filename][0]
-        scene_filenames = [filename for filename in filenames if filename != char_filename]
+        char_filename = [filename for filename in filenames if "chars" in filename][0]
+        scene_filenames = [
+            filename for filename in filenames if filename != char_filename
+        ]
 
         return char_filename, scene_filenames
 
-    def _compile_character_file(self, filepath: str, output_folder: str) -> None:
-        output_filepath = path.abspath(path.join(output_folder, path.basename(filepath).replace('.txt', '.json')))
-        result = self._compile_file('char', filepath)
+    def _get_abspath(self, filepath: str, script_path: str) -> str:
+        return path.join(path.abspath(script_path), filepath)
 
-        with open(output_filepath, 'w+') as f:
+    def _compile_character_file(self, filepath: str, output_folder: str) -> None:
+        output_filepath = path.abspath(
+            path.join(output_folder, path.basename(filepath).replace(".txt", ".json"))
+        )
+        result = self._compile_file("char", filepath)
+
+        with open(output_filepath, "w+") as f:
             json.dump(result, f)
 
     def _compile_scene_file(self, filepath: str, output_folder: str) -> None:
-        output_filepath = path.abspath(path.join(output_folder, path.basename(filepath).replace('.txt', '.json')))
-        result = self._compile_file('scene', filepath)
+        output_filepath = path.abspath(
+            path.join(output_folder, path.basename(filepath).replace(".txt", ".json"))
+        )
+        result = self._compile_file("scene", filepath)
 
-        with open(output_filepath, 'w+') as f:
+        with open(output_filepath, "w+") as f:
             json.dump(result, f)
 
-    def _compile_file(self, type_file: str, filepath:str) -> Attribute:
-        with open(filepath, 'r') as f:
+    def _compile_file(self, type_file: str, filepath: str) -> Attribute:
+        with open(filepath, "r") as f:
             tokenized = self.parsers[type_file].tokenizer.tokenize(f.read())
         return self.parsers[type_file].parser.parse(tokenized)
--- config/character.py
+++ config/character.py
@@ -1,43 +1,43 @@
 from config.reader import parse_actions, parse_goto, parse_tokens, reader_config
 from lrparser.utils.table_descriptor import ReduceFunc
 
-updateCharacterList: ReduceFunc = lambda CS, C: {
+update_character_list: ReduceFunc = lambda CS, C: {
     "list": [*CS.get("list"), C.get("char")]
 }
-createCharacterList: ReduceFunc = lambda C1, C2: {
+create_character_list: ReduceFunc = lambda C1, C2: {
     "list": [C1.get("char"), C2.get("char")]
 }
 
-updateSpriteList: ReduceFunc = lambda SS, S: {
+update_sprite_list: ReduceFunc = lambda SS, S: {
     "list": [*SS.get("list"), S.get("sprite")]
 }
-createSpriteList: ReduceFunc = lambda S1, S2: {
+create_sprite_list: ReduceFunc = lambda S1, S2: {
     "list": [S1.get("sprite"), S2.get("sprite")]
 }
 
-createCharacterWithoutSprites: ReduceFunc = lambda c, n: {
+create_character_without_sprites: ReduceFunc = lambda c, n: {
     "char": {"name": n.get("val")}
 }
-createCharacter: ReduceFunc = lambda c, n, op, SS, cl: {
+create_character: ReduceFunc = lambda c, n, op, SS, cl: {
     "char": {"name": n.get("val"), "sprites": SS.get("list")}
 }
 
-createSprite: ReduceFunc = lambda f, e, p: {
+create_sprite: ReduceFunc = lambda f, e, p: {
     "sprite": {"field": f.get("val"), "path": p.get("val")[1:-1]}
 }
 
-prepareResult: ReduceFunc = lambda CS, e: {"res": CS.get("list")}
+prepare_result: ReduceFunc = lambda CS, e: {"res": CS.get("list")}
 
 
 funcs: dict[str, ReduceFunc] = {
-    "updateCharacterList": updateCharacterList,
-    "createCharacterList": createCharacterList,
-    "updateSpriteList": updateSpriteList,
-    "createSpriteList": createSpriteList,
-    "createCharacterWithoutSprites": createCharacterWithoutSprites,
-    "createCharacter": createCharacter,
-    "createSprite": createSprite,
-    "prepareResult": prepareResult,
+    "updateCharacterList": update_character_list,
+    "createCharacterList": create_character_list,
+    "updateSpriteList": update_sprite_list,
+    "createSpriteList": create_sprite_list,
+    "createCharacterWithoutSprites": create_character_without_sprites,
+    "createCharacter": create_character,
+    "createSprite": create_sprite,
+    "prepareResult": prepare_result,
 }
 
 json_config = reader_config("config/character_config.json")

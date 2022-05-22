09a8e8000950898e4d264c656dc8401ff19020c0
Resinchen <alex1410alex@gmail.com>
2022-05-22 21:20:39 +0500

    add test tokenizer

--- tests/unit/lrparser/test_tokenizer.py
+++ tests/unit/lrparser/test_tokenizer.py
@@ -3,18 +3,32 @@ import re
 import pytest
 
 from lrparser.tokenizer import Tokenizer
-from lrparser.utils.tokenizer import TokenType
+from lrparser.utils.tokenizer import TokenType, Token
+
+
+TOKEN_TYPES = {
+    'kek': TokenType(name='kek', pattern=re.compile('kek')),
+    'lol': TokenType(name='lol', pattern=re.compile('lol')),
+    'none': TokenType(name='none', pattern=re.compile('[ \n]+')),
+    'eof': TokenType(name='eof', pattern=re.compile(''))
+}
 
 
 @pytest.fixture()
 def tokenizer() -> Tokenizer:
-    return Tokenizer([
-        TokenType(name='kek', pattern=re.compile('kek')),
-        TokenType(name='lol', pattern=re.compile('lol')),
-        TokenType(name='open', pattern=re.compile('{')),
-        TokenType(name='close', pattern=re.compile('}')),
-        TokenType(name='none', pattern=re.compile('[ \n\t]+')),
-    ])
+    return Tokenizer(list(TOKEN_TYPES.values()))
+
 
 class TokenizerTest:
-    ...
+    def test_tokenize(self, tokenizer):
+        expected_tokens = [
+            Token(TOKEN_TYPES['lol'], value='lol'),
+            Token(TOKEN_TYPES['none'], value=' '),
+            Token(TOKEN_TYPES['kek'], value='kek'),
+            Token(TOKEN_TYPES['none'], value='    \n'),
+            Token(TOKEN_TYPES['kek'], value='kek'),
+            Token(TOKEN_TYPES['eof'], value='eof')
+        ]
+        text = 'lol kek    \nkek'
+
+        assert tokenizer.tokenize(text) == expected_tokens
